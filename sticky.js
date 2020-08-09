90a997b6e7d31cbd142a529ec5e71c73c03fca4d
Yogeshwar Sharma <54261899+yogeshsharma4477@users.noreply.github.com>
2020-08-10 02:25:56 +0530

    Add files via upload

---
+++ sticky.js
@@ -0,0 +1,14 @@
+// window.onscroll = function() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+
+
+// let navbar = document.getElementById("navbar");
+
+// let sticky=navbar.offsetTop;
+
+
